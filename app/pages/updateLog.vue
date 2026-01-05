8be39ac06f42e8645277813050b950ce76d29488
name <email@example.com>
2026-01-05 12:28:06 +0800

    tt

--- app/pages/updateLog.vue
+++ app/pages/updateLog.vue
@@ -55,6 +55,10 @@ import { ref, computed } from "vue";
 // 构建列表
 const infoList = ref([
   {
+    time: "2026-1-5",
+    content: "添加了图片的占位符,解决了图片加载后布局偏移的问题",
+  },
+  {
     time: "2026-1-3",
     content: "新年第一次更新,优化了移动端下拉菜单栏的性能问题",
   },
