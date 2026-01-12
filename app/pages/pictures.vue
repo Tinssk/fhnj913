6d0cc6fb1a542342e38ceeed6e8dc341a0f725b4
name <email@example.com>
2026-01-12 15:08:00 +0800

    细节优化

--- app/pages/pictures.vue
+++ app/pages/pictures.vue
@@ -26,7 +26,8 @@
       <div v-for="col in columns" class="flex-1 ">
         <div v-for="img in col" :key="img"
           class="groupN group mb-4  relative  overflow-hidden rounded-lg transition-all duration-300 bg-no-repeat bg-cover "
-          :class="imgStates[img].loaded ? 'loaded' : 'blur-sm'" @click.stop="handleImageClickShow(img)" :style="{
+          :class="imgStates[img].loaded ? 'loaded shadow-lg' : 'blur-sm border-4 border-green-400 shadow-md'"
+          @click.stop="handleImageClickShow(img)" :style="{
             backgroundImage: `url('${imgStates[img].bg_url}')`,
             aspectRatio: `${imgSizeMap[img].width}/${imgSizeMap[img].height}`
           }">
@@ -261,6 +262,10 @@ const handleImageClickShow = (imgUrl) => {
   /* 可选 */
   gap: 16px;
   /* 列间距 */
+}
+
+.groupN {
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 .groupN::before {
