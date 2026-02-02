a220af1f87692ef6b860c88bfa9b65744e94a23c
name <email@example.com>
2026-02-02 10:36:35 +0800

    折花笺内容补充

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-04-01",
   devtools: { enabled: false },
   devServer: {
-    host: "192.168.10.2",
+    host: "192.168.10.7",
     port: 3000,
   },
 
